4ac89dd4bdbcaa917b1a32a4ebb742e846d24c33
montassier-paul <paul.montassier.pro@gmail.com>
2022-08-03 11:29:57 +0200

    patch 1 for heroku error

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const tablesRoute = require('./routes/tables')
 const trendsRoute = require('./routes/trends')
 
 dotenv.config();
-const port = process.env.PORT || 3000
+const port = process.env.PORT 
 
 
 const app = express()
